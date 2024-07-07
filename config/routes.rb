d99daaa5998621649d29cb865d6e485d25ccbbde
dpatel222 <deeppatel2@rrc.ca>
2024-07-07 17:06:18 -0500

    Setting up user with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { regestrations: 'users/regestrations' }
   root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
