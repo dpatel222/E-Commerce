ec516b9aa3cc9aee3bb56ae057b7dc60586fa07c
dpatel222 <deeppatel2@rrc.ca>
2024-07-08 16:09:32 -0500

    User can not log out

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+import { Turbo } from "@hotwired/turbo-rails";
+import { Rails } from "@rails/request.js";
+
+Turbo.start();
+Rails.start();
