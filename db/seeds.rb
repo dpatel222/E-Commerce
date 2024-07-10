526450e59f7fe39ce9ba84fd861378bce9c7982a
dpatel222 <deeppatel2@rrc.ca>
2024-07-10 09:19:43 -0500

    User can now add a address during login or afterwards

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,41 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-=begin Province.create([
-                  { name: 'Alberta', GST: 5.00, PST: 0.00, HST: 0.00 },
-                  { name: 'British Columbia', GST: 5.00, PST: 7.00, HST: 0.00 },
-                  { name: 'Manitoba', GST: 5.00, PST: 7.00, HST: 0.00 },
-                  { name: 'New Brunswick', GST: 0.00, PST: 0.00, HST: 15.00 },
-                  { name: 'Newfoundland and Labrador', GST: 0.00, PST: 0.00, HST: 15.00 },
-                  { name: 'Northwest Territories', GST: 5.00, PST: 0.00, HST: 0.00 },
-                  { name: 'Nova Scotia', GST: 0.00, PST: 0.00, HST: 15.00 },
-                  { name: 'Nunavut', GST: 5.00, PST: 0.00, HST: 0.00 },
-                  { name: 'Ontario', GST: 0.00, PST: 0.00, HST: 13.00 },
-                  { name: 'Prince Edward Island', GST: 0.00, PST: 0.00, HST: 15.00 },
-                  { name: 'Quebec', GST: 5.00, PST: 9.975, HST: 0.00 },
-                  { name: 'Saskatchewan', GST: 5.00, PST: 6.00, HST: 0.00 },
-                  { name: 'Yukon', GST: 5.00, PST: 0.00, HST: 0.00 }
-                ])
-=end
+# Province.create([
+#                   { name: 'Alberta', GST: 5.00, PST: 0.00, HST: 0.00 },
+#                   { name: 'British Columbia', GST: 5.00, PST: 7.00, HST: 0.00 },
+#                   { name: 'Manitoba', GST: 5.00, PST: 7.00, HST: 0.00 },
+#                   { name: 'New Brunswick', GST: 0.00, PST: 0.00, HST: 15.00 },
+#                   { name: 'Newfoundland and Labrador', GST: 0.00, PST: 0.00, HST: 15.00 },
+#                   { name: 'Northwest Territories', GST: 5.00, PST: 0.00, HST: 0.00 },
+#                   { name: 'Nova Scotia', GST: 0.00, PST: 0.00, HST: 15.00 },
+#                   { name: 'Nunavut', GST: 5.00, PST: 0.00, HST: 0.00 },
+#                   { name: 'Ontario', GST: 0.00, PST: 0.00, HST: 13.00 },
+#                   { name: 'Prince Edward Island', GST: 0.00, PST: 0.00, HST: 15.00 },
+#                   { name: 'Quebec', GST: 5.00, PST: 9.975, HST: 0.00 },
+#                   { name: 'Saskatchewan', GST: 5.00, PST: 6.00, HST: 0.00 },
+#                   { name: 'Yukon', GST: 5.00, PST: 0.00, HST: 0.00 }
+#                 ])
 
-Product.create(
-  name: 'My Product',
-  category: 'Category 1',
-  price: '10'
-)
+require 'csv'
+Product.destroy_all
+
+Category.destroy_all
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product|
+  category_name = product['category']
+  categories = Category.find_or_create_by(name: category_name)
+
+  Product.create(
+    name: product['name'],
+    price: product['price'],
+    description: product['description'],
+    stock_quantity: product['stock quantity'],
+    category: categories
+  )
+end
