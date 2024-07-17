30b48d4c8cc41d6b2b4abb06e6f4f5dc5fc785b1
dpatel222 <deeppatel2@rrc.ca>
2024-07-17 13:59:24 -0500

    Added button to add products to cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -37,4 +37,33 @@ class ProductsController < ApplicationController
   #   def initialize_session
   #     session[:cart] ||= []
   #   end
+  def add_to_cart
+    product_id = params[:id].to_i
+    product = Product.find(product_id)
+
+    if current_user
+      # Find or create a cart item for the current user
+      cart_item = current_user.cart_items.find_or_initialize_by(product:)
+      cart_item.quantity = cart_item.quantity.to_i + 1
+      cart_item.save
+    else
+      add_product_to_cart(product_id)
+    end
+
+    flash[:notice] = 'Product has been added to your cart.'
+    redirect_to products_path
+  end
+
+  private
+
+  def add_product_to_cart(product_id)
+    session[:cart] ||= []
+    existing_item = session[:cart].find { |item| item['product_id'] == product_id }
+
+    if existing_item
+      existing_item['quantity'] += 1
+    else
+      session[:cart] << { 'product_id' => product_id, 'quantity' => 1 }
+    end
+  end
 end
