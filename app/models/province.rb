f0021054d39916312dbc1136bde012dddfd9b150
dpatel222 <deeppatel2@rrc.ca>
2024-07-24 10:35:37 -0500

    Admin can now change the tax rates

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,3 +1,11 @@
 class Province < ApplicationRecord
   has_many :users
+
+  def self.ransackable_associations(auth_object = nil)
+    ['users']
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[GST HST PST created_at id id_value name updated_at]
+  end
 end
